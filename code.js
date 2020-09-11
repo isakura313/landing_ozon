f6cb07d867bc37432f716f0a4235e8c3a07f40d0
Paul <isakura313@gmail.com>
2020-09-11 20:58:29 +0300

    asdasd

--- code.js
+++ code.js
@@ -8,4 +8,5 @@ select.onblur = () => {
 let nav_mobile = document.querySelector(".nav_mobile");
 adaptive_menu.onclick = () =>{
     nav_mobile.classList.toggle("see_helper");
+    nav_mobile.classList.toggle("animate__flipInY");
 }
