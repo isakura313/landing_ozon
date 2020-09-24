f4fad84eeb23ddcc2b666800c4d21e1d844e93d3
Oleg Kishinskiy <oleg@kishinskiy.ru>
2020-09-24 05:39:41 +0300

    add structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 "use strict";
 
-const gulp = require("gulp");
-const webpack = require("webpack-stream");
-const browsersync = require("browser-sync");
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-const dist = "./dist/";
+
+const dist = "./docs/";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
@@ -12,84 +14,36 @@ gulp.task("copy-html", () => {
                 .pipe(browsersync.stream());
 });
 
-gulp.task("build-js", () => {
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist))
+
+gulp.task("copy-images", () => {
+    return gulp.src("./src/imgages/**/*.*")
+                .pipe(gulp.dest(dist + "/images"))
                 .on("end", browsersync.reload);
 });
 
-gulp.task("copy-assets", () => {
-    return gulp.src("./src/assets/**/*.*")
-                .pipe(gulp.dest(dist + "/assets"))
-                .on("end", browsersync.reload);
+gulp.task('server', function() {
+  browserSync.init({
+      server: {
+          baseDir: dist
+      }
+  });
 });
 
-gulp.task("watch", () => {
-    browsersync.init({
-		server: "./dist/",
-		port: 4000,
-		notify: true
-    });
-    
-    gulp.watch("./src/index.html", gulp.parallel("copy-html"));
-    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
-    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+
+gulp.task('styles', function(){
+  return gulp.src("src/sass/**/*.+(scss|sass)")
+          .pipe(sass().on('error', sass.logError))
+          .pipe(autoprefixer())
+          .pipe(cleanCSS({compatibility: 'ie8'}))
+          .pipe(gulp.dest(dist + "css"))
+          .pipe(browserSync.stream());
 });
 
-gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
-
-gulp.task("build-prod-js", () => {
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'production',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist));
+gulp.task('watch', function(){
+  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+  gulp.watch("src/images/**/*.*", gulp.parallel("copy-images"));
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task("default", gulp.parallel("watch", "build"));
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
