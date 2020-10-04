b85c1b3038451b2f1808388e8add543086049450
Oleg Kishinskiy <oleg@kishinskiy.ru>
2020-10-04 19:13:22 +0300

    add burger

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$('.mobile__menu-btn').on('click', function(e){
+    e.preventDefault;
+    $(this).toggleClass('mobile__menu-btn_active');
+    $('.mobile__menu').toggleClass('mobile__menu_active')
+});
+
