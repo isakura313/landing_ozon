a3d94680da02ec50f4737a375f2b8a84fceaca99
Oleg Kishinskiy <oleg@kishinskiy.ru>
2020-10-04 18:00:38 +0300

    make select

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let select = function () {
+    let selectHeder = document.querySelectorAll('.select__header');
+
+    let selectItem = document.querySelectorAll('select__item');
+
+    selectHeder.forEach(item =>{
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item =>{
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+
+    function selectChoose(){
+        let text = this.innerText,
+        currentText = this.closest('.select').querySelector('.select__current');
+        currentText.innerText = text
+    }
+}
+
+select();
